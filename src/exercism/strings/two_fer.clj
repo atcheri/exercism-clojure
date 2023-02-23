452e45f17ecf6ad9215bf9397b3a076e0198f2fa
Atcheri Atch <atcheri@gmail.com>
2023-02-23 06:45:35 +0100

    feat: two-fer

---
+++ src/exercism/strings/two_fer.clj
@@ -0,0 +1,15 @@
+(ns exercism.strings.two-fer
+  (:require [clojure.string :as str]))
+
+
+(defn two-fer
+  ([] (two-fer "you"))
+  ([name]
+   (str "One for "
+        (cond
+          (str/blank? name) "you"
+          :else name)
+        ", one for me.")))
+
+
+(two-fer "")
