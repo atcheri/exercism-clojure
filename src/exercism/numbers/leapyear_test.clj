d5694f99b4b325c2d8ddbb7a8a994dbfe38b1cfe
Atcheri Atch <atcheri@gmail.com>
2023-10-16 15:08:17 +0200

    feat: add leap year solutions

---
+++ src/exercism/numbers/leapyear_test.clj
@@ -0,0 +1,31 @@
+(ns exercism.numbers.leapyear-test
+  (:require [clojure.test :refer [deftest is]]
+            [exercism.numbers.leapyear :refer [leap-year?]]))
+
+
+(deftest year-not-divisible-by-4
+  (is (not (leap-year? 2015))))
+
+(deftest year-divisible-by-2-but-not-4
+  (is (not (leap-year? 1970))))
+
+(deftest year-divisible-by-4-but-not-100
+  (is (leap-year? 1996)))
+
+(deftest year-divisible-by-4-and-5
+  (is (leap-year? 1960)))
+
+(deftest year-divisible-by-100-but-not-400
+  (is (not (leap-year? 2100))))
+
+(deftest year-divisible-by-100-but-not-by-3
+  (is (not (leap-year? 1900))))
+
+(deftest year-divisible-by-400
+  (is (leap-year? 2000)))
+
+(deftest year-divisible-by-400-but-not-125
+  (is (leap-year? 2400)))
+
+(deftest year-divisible-by-200-but-not-by-400
+  (is (not (leap-year? 1800))))
